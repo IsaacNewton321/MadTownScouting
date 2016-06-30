ecea35ad166b6303f48b223ce2fb2ac1d3161996
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-06-30 15:41:40 -0700

    Added response to search string
    Made it so that if an extra is attached to the activity under "SEARCH" the activity will check if it has a t or an m. In the case of t, the number attached to the string will be added to the string search, and the query for the listview will be a query of all data of that team. In the case of m, the number attached to the string will be attached to the search string, and the query for the listview will be a query for all of the data of that certain match number.

--- app/src/main/java/com/example/robotics3/madtownscouting/SendData.java
+++ app/src/main/java/com/example/robotics3/madtownscouting/SendData.java
@@ -238,6 +238,14 @@ public class SendData extends AppCompatActivity {
         Intent intent = getIntent();
         if(intent.getStringExtra("ME") != null){
             query = intent.getStringExtra("ME") + intent.getStringExtra("TEAM_NUMBER");
+        }else if(intent.getStringExtra("SEARCH") != null){
+            if(intent.getStringExtra("SEARCH").toLowerCase().contains("m")){
+                String search = intent.getStringExtra("SEARCH").toLowerCase().replace("m","");
+                query = "SELECT * FROM MatchScouting WHERE matchtype_number = " + search;
+            }else if(intent.getStringExtra("SEARCH").toLowerCase().contains("t")){
+                String search = intent.getStringExtra("SEARCH").toLowerCase().replace("t","");
+                query = "SELECT * FROM MatchScouting WHERE teamNumber = " + search;
+            }
         }
         myDB = openOrCreateDatabase("FRC", MODE_PRIVATE, null);
         c1 = myDB.rawQuery(query, null);
