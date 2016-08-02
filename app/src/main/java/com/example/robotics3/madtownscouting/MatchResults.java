e7d46de37ac6d9898f2a1df9b83d7030a97e4439
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-08-02 16:55:20 -0700

    Update on edit

--- app/src/main/java/com/example/robotics3/madtownscouting/MatchResults.java
+++ app/src/main/java/com/example/robotics3/madtownscouting/MatchResults.java
@@ -394,6 +394,7 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             String match = data.getStringExtra("MATCH_NUMBER");
             String teamN = data.getStringExtra("TEAM_NUMBER");
             Toast.makeText(getApplicationContext(),"Team " + teamN + ", Match " + match + "updated.", Toast.LENGTH_SHORT).show();
+            matchAdapter.notifyDataSetChanged();
         }
         if (resultCode == Activity.RESULT_CANCELED) {
             
