06e2e8fbe46985d0c4947e21d9d027d1957644cc
IsaacNewton321 <isaacduartevilla@gmail.com>
2016-09-06 17:09:08 -0700

    working on uploading img

--- app/src/main/java/com/example/robotics3/madtownscouting/UploadPicture.java
+++ app/src/main/java/com/example/robotics3/madtownscouting/UploadPicture.java
@@ -1,5 +1,6 @@
 package com.example.robotics3.madtownscouting;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,9 +11,20 @@ import android.media.ExifInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class UploadPicture extends AppCompatActivity {
 
@@ -21,15 +33,19 @@ public class UploadPicture extends AppCompatActivity {
     SQLiteDatabase myDB = null;
     Cursor c;
     String id;
-    String pathToPic;
+    String selectedFilePath;
     Bitmap bp;
     ExifInterface ei;
+    ProgressDialog dialog;
+    TextView message;
+    private String SERVER_URL = "http://www.gorohi.com/1323/data.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_picture);
         picUploadButton = (Button) findViewById(R.id.picUploadButton);
         picPreview = (ImageView) findViewById(R.id.picUploadPreview);
+
         String[] columns = new String[]{
                 "_id",
                 "teamNumber",
@@ -42,9 +58,9 @@ public class UploadPicture extends AppCompatActivity {
         c.moveToFirst();
         try {
             if(c.getCount() > 0) {
-                pathToPic = c.getString(c.getColumnIndex("pic1"));
-                bp = BitmapFactory.decodeFile(pathToPic);
-                ei = new ExifInterface(pathToPic);
+                selectedFilePath = c.getString(c.getColumnIndex("pic1"));
+                bp = BitmapFactory.decodeFile(selectedFilePath);
+                ei = new ExifInterface(selectedFilePath);
             }
         } catch (Exception e) {
             Log.d("ERROR", e.toString());
@@ -66,6 +82,26 @@ public class UploadPicture extends AppCompatActivity {
         }
 
         picPreview.setImageBitmap(bp);
+
+        picUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //on upload button Click
+                if(selectedFilePath != null){
+                    dialog = ProgressDialog.show(UploadPicture.this, "", "Uploading File...", true);
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //creating new thread to handle Http Operations
+                            uploadFile(selectedFilePath);
+                        }
+                    }).start();
+                }else{
+                    Toast.makeText(UploadPicture.this,"Please choose a File First", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public static Bitmap rotateImage(Bitmap source, float angle) {
@@ -73,5 +109,107 @@ public class UploadPicture extends AppCompatActivity {
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix,
                 true);
+    }
+    public int uploadFile(final String selectedFilePath){
+
+        int serverResponseCode = 0;
+
+        HttpURLConnection connection;
+        DataOutputStream dataOutputStream;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File selectedFile = new File(selectedFilePath);
+
+
+        String[] parts = selectedFilePath.split("/");
+        final String fileName = parts[parts.length-1];
+
+        if(!selectedFile.isFile()){
+            dialog.dismiss();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    message.setText("Source file doesn't exist: " + selectedFilePath);
+                }
+            });
+            return 0;
+        }else{
+            try{
+                FileInputStream fileInputStream = new FileInputStream(selectedFile);
+                URL url = new URL(SERVER_URL);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setUseCaches(false);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("ENCTYPE", "multipart/form-data");
+                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                connection.setRequestProperty("uploaded_file", selectedFilePath);
+
+                dataOutputStream = new DataOutputStream(connection.getOutputStream());
+
+                dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + selectedFilePath + "\"" + lineEnd);
+
+                dataOutputStream.writeBytes(lineEnd);
+
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while(bytesRead > 0){
+                    dataOutputStream.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesRead, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+
+                dataOutputStream.writeBytes(lineEnd);
+                dataOutputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                serverResponseCode = connection.getResponseCode();
+                final String serverResponseMessage = connection.getResponseMessage();
+
+                if(serverResponseCode == 200){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            message.setText(serverResponseMessage);
+                        }
+                    });
+                }
+
+                fileInputStream.close();
+                dataOutputStream.flush();
+                dataOutputStream.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(UploadPicture.this, "File Not Found", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                Toast.makeText(UploadPicture.this, "URL error!", Toast.LENGTH_SHORT).show();
+            } catch (java.io.IOException e){
+                e.printStackTrace();
+                Toast.makeText(UploadPicture.this, "Cannot Read/Write File!", Toast.LENGTH_SHORT).show();
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+        }
     }
 }
